861d7ddb881975f4faee9e69df1a5aae934a4849
raywisex24-collab <raywisex24@gmail.com>
2026-09-06 19:40:03 +0100

    Fix music API CORS

--- index.js
+++ index.js
@@ -14,13 +14,20 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+    ],
     methods: [
       "GET",
       "POST",
       "PUT",
       "DELETE",
       "OPTIONS",
+    ],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
     ],
   })
 );
