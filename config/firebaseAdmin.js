fe548224ae8395c1fde0038dfe398c978976cd3d
raywisex24-collab <raywisex24@gmail.com>
2026-09-07 09:43:39 +0100

    many chages considering music

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,11 +1,19 @@
-const admin = require("firebase-admin");
+const { getApps, initializeApp, cert } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
+const { getFirestore, FieldValue } = require("firebase-admin/firestore");
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(
+if (getApps().length === 0) {
+  initializeApp({
+    credential: cert(
       JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
     ),
   });
 }
+
+const admin = {
+  auth: getAuth,
+  firestore: getFirestore,
+  FieldValue,
+};
 
 module.exports = admin;
